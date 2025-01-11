b1860348e0d19cf196b19999cf61292e9cc78a46
Leonardo Sposito <oliveiraleosilva87@gmail.com>
2025-01-11 17:30:15 -0300

    added lists.py

---
+++ python-scripts/lists.py
@@ -0,0 +1,64 @@
+# create a list with numbers from 1 to 10
+my_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+# print the list
+print(my_list)
+
+# sort the list in ascending order
+my_list.sort()
+
+# print the sorted list
+print(my_list)
+
+# create a list with integers from 1 to 10
+integers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print the integers list
+print(integers_list)
+
+# sort the integers list in ascending order
+integers_list.sort()
+
+# print the sorted integers list
+print(integers_list)
+
+# print the integers list in descending order
+integers_list.sort(reverse=True)
+print(integers_list)
+
+# back the list to ascending order
+integers_list.sort()
+print(integers_list)
+
+# count the quantity of elements in the list
+print(len(integers_list))
+
+# add the next integer to the integers list
+integers_list.append(11)
+print(integers_list)
+
+# count the quantity of elements in the list
+print(len(integers_list))
+
+# remove the first element from the list
+integers_list.pop(0)
+print(integers_list)
+
+# remove the last element from the list
+integers_list.pop()
+
+# print the first element of the list
+print(integers_list[0])
+
+# print the last element of the list
+print(integers_list[-1])
+
+# add a duplicate integer to the list
+integers_list.append(3)
+
+# print the integers list
+print(integers_list)
+
+
+
+
