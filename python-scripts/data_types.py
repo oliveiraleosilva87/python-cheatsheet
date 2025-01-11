70b3961b3ecfde8a6bd9328a2aa67b863d82fc69
Leonardo Sposito <oliveiraleosilva87@gmail.com>
2025-01-11 17:25:03 -0300

    added data_types.py

---
+++ python-scripts/data_types.py
@@ -0,0 +1,38 @@
+# print a hello world
+print("Hello World")
+
+# print a integer
+print(10)
+
+# print a integer as a string
+print("10")
+
+# print a float
+print(10.2)
+
+# print a float as a string
+print("10.2")
+
+# print a boolean
+print(True)
+
+# print a boolean as a string
+print("True")
+
+# print a list
+print([1, 2, 3, 4, 5])
+
+# print dict
+print({"name": "John", "age": 30})
+
+# print a tuple
+print((1, 2, 3, 4, 5))
+
+# print a set
+print({1, 2, 3, 4, 5})
+
+# print a multiline string
+print("""
+This is a multiline string
+that spans multiple lines.
+""")
